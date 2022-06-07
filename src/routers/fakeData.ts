7b0acc8eefa5baa8c9bdd9ac97ee6ec136855b5b
David Johnson <dcjohnson85@gmail.com>
2022-06-07 23:06:50 +0100

    Added ability to add collections and entries into in memory storage

--- src/routers/fakeData.ts
+++ src/routers/fakeData.ts
@@ -1,5 +1,6 @@
 import KoaRouter from "koa-router";
 import { StatusCodes } from 'http-status-codes';
+import {v4} from 'uuid';
 
 const router = new KoaRouter({
     prefix: '/fake-data'
@@ -8,17 +9,38 @@ const router = new KoaRouter({
 const collections: Map<string, Map<string, any>> = new Map(); 
 
 router.get('/', (ctx) => {
-    ctx.body = "hit dater faker"
+    ctx.body = [...collections.keys()];
 });
 
-router.post('/:collection', (ctx) => {
-    console.log("input: ", ctx.request.body);
+router.get('/:collection', (ctx) => {
+    const collection = collections.get(ctx.params.collection);
+
+    ctx.body = {
+        collectionName: ctx.params.collection,
+        values: [...collection.entries()].map((entry) => {
+            return {id: entry[0], value: entry[1]}
+        })
+    }
+});
+
+router.get('/:collection/:id', (ctx) => {
+    const collection = collections.get(ctx.params.collection);
+    const entry = collection?.get(ctx.params.id);
+
+    ctx.status = StatusCodes.OK;
+    ctx.body = entry || "no value";
+});
+
+router.post('/:collectionKey', (ctx) => {
+    const {collectionKey} = ctx.params;
+    const collection = collections.get(collectionKey) || new Map();
     let { id, ...payload } = ctx.request.body; 
 
-    id = id || 'aaaa';
+    id = id || v4();
     
-    console.log(id, payload);
-    ctx.request.body
+    const lookupValue = collection.get(id) || payload;
+    collection.set(id, lookupValue);
+    collections.set(collectionKey, collection);
 
     ctx.body = {
         url: `https://localhost:3000/fake-data/${ctx.params.collection}`,
